1ee3c1e68b66fb197039063b4ec8cdca890327f4
Artem Trubacheev <Almaz5200@gmail.com>
2020-01-16 22:12:23 +0300

    Initial Commit

---
+++ DiscussionMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiscussionMaker
+//
+//  Created by Artem Trubacheev on 16.01.2020.
+//  Copyright © 2020 Artem Trubacheev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
